1e981695eb53d19b8c2865c49abbd052edfd2c74
wapxaz <wapxa@rambler.ru>
2022-06-07 00:01:50 +0300

    добавляю файлы в проект, что изучал и разрабатывал локально

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Body() {
+  return (
+      <div className="bodyApp">
+        Тут основная часть приложения.
+      </div>
+    );
+}
+
+export default Body;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+      <div>
+        Рады будем увидеть Вас снова, приходите ещё.
+      </div>
+    );
+}
+
+export default Footer;

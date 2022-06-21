d759602bd22b146039b07f43018d469ce22b918a
wapxaz <wapxa@rambler.ru>
2022-06-22 00:45:27 +0300

    Начал делать страницу Dialogs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
 import Profile from './components/Profile/Profile.jsx';
+import Dialogs from './components/Dialogs/Dialogs.jsx';
 
 function App() {
   return (
     <div className="App grid">
       <Header />
       <NavBar />
-      <Profile />
+      <div className="app-wrapper-content">
+        <Dialogs />
+       </div>
+      {/*<Profile />*/}     
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import profileCss from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts.jsx';
 
 const Profile = () => {
-	return <div className={profileCss.content}>
+	return <div>
         <div>
           <img src="https://s1.cdn.autoevolution.com/images/news/compact-suv-comparison-finds-bmw-x1-is-better-than-mercedes-gla-and-audi-q3-115365_1.jpg" />
         </div>
